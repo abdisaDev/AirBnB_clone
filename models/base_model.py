fea3f0862f5faafcbf17c8da94e86080502f3a92
Abdisa Dev <AbdisaDev@gmail.com>
2024-05-29 10:44:54 +0300

    updated at

--- models/base_model.py
+++ models/base_model.py
@@ -6,23 +6,21 @@ from datetime import datetime
 
 class BaseModel:
     """ BaseModel Class """
-    __date_iso_formated = datetime.now().isoformat()
-    __date = datetime.now()
 
     def __init__(self) -> None:
         self.id = str(uuid.uuid4())
-        self.created_at = self.__date
-        self.updated_at = self.__date
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
 
     def save(self):
-        self.updated_at = self.__date
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         dict = self.__dict__
         dict["__class__"] = self.__class__.__name__
-        dict["created_at"] = self.__date_iso_formated
-        dict["updated_at"] = self.__date_iso_formated
+        dict["created_at"] = datetime.now().isoformat()
+        dict["updated_at"] = datetime.now().isoformat()
         return dict
