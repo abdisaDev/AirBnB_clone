62727796b8318978d664b77b6757da913c14d4c5
Abdisa Dev <AbdisaDev@gmail.com>
2024-05-29 10:46:44 +0300

    documented

--- models/base_model.py
+++ models/base_model.py
@@ -8,17 +8,21 @@ class BaseModel:
     """ BaseModel Class """
 
     def __init__(self) -> None:
+        """ BaseModel Class Initailizer """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
+        """ string instance """
         return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
 
     def save(self):
+        """ capture updated time """
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """ serialize the data """
         dict = self.__dict__
         dict["__class__"] = self.__class__.__name__
         dict["created_at"] = datetime.now().isoformat()
